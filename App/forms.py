7e08aa64c5c70d43c2a3dcba72fdcec33105c7c0
Tejas <tejastarghale@gmail.com>
2023-07-03 22:08:35 +0530

    'Message'

--- App/forms.py
+++ App/forms.py
@@ -10,5 +10,5 @@ class UserRegistration(forms.ModelForm):
         widgets = {
                 "name" : forms.TextInput(attrs={'class':'form-control'}),
                 "email" : forms.EmailInput(attrs={'class':'form-control'}),
-                'password' : forms.PasswordInput(render_value=True, attrs={'class':'form-control','error_message':'Hi There'}),
+                'password' : forms.PasswordInput(render_value=True, attrs={'class':'form-control'}),
         }
